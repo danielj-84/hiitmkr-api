b1e83de8fe080be4431cb8e99da84cdfe449645f
danielj-84 <danieldimatteo123@gmail.com>
2022-07-01 12:31:08 -0400

    intial commit

---
+++ server.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const SpotifyWebApi = require("spotify-web-api-node");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const helmet = require("helmet");
+
+//Import config
+require("dotenv").config();
+// const PORT = process.env.PORT || 3001;
+
+//Create Express App
+const app = express();
+
+//Establish middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Set Security Configs
+app.use(helmet());
+
+//Establish refresh Token route
+app.post("/refresh", (req, res) => {
+  const refreshToken = req.body.refreshToken;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.SPOTIFY_REDIRECT_URI,
+    clientId: process.env.SPOTIFY_CLIENT_ID,
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    refreshToken,
+  });
+  spotifyApi
+    .refreshAccessToken()
+    .then((data) => {
+      res.json({
+        accessToken: data.body.accessToken,
+        expiresIn: data.body.expiresIn,
+      });
+    })
+    .catch(() => {
+      res.sendStatus(400);
+    });
+});
+
+//Establish login route
+app.post("/login", (req, res) => {
+  const code = req.body.code;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+  });
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch(err => {
+        console.log(err)
+      res.sendStatus(400);
+    });
+});
+
+//Server
+// app.listen(PORT, () => {
+//   console.log(`App is running on PORT ${PORT}`);
+// });
+app.listen(3001)
